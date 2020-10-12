d41e5f3a8a385b62277ec86e8c2aa07332e9e7f7
KvasLover <vadimkurb22@gmail.com>
2020-10-12 09:53:01 +0300

    Добавьте файлы проекта.

---
+++ laba3/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    class House
+    {
+        public int id, floor, amount, flat_number, time;
+        public string square, street, building_type;
+        public House(int id, int floor, string square, int amount, int flat_number, int time, string street, string building_type) //конструктор
+        {
+            this.id = id;
+            this.floor = floor;
+            this.square = square;
+            this.amount = amount;
+            this.flat_number = flat_number;
+            this.time = time;
+            this.street = street;
+            this.building_type = building_type;
+        }
+        public void Info() //метод
+        {
+            Console.WriteLine($"{id} {floor} {square} {amount} {flat_number} {time} {street} {building_type}");
+        }
+        public int Age(int time)
+        {
+            int time1 = 2020 - time;
+            return time1;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter id: "); 
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter floor: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter square: ");
+            string c = Console.ReadLine();
+            Console.Write($"Enter amount: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter flat_number: ");
+            int e = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter expluatation time: ");
+            int f = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter street: ");
+            string g = Console.ReadLine();
+            Console.Write($"Enter building_type: ");
+            string h = Console.ReadLine();
+            House house = new House(a,b,c,d,e,f,g,h);
+            house.Info();
+            int res = house.Age(f);
+            Console.WriteLine($"Age of building: {res}");
+        }
+    }
+}
